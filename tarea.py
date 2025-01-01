5002bbfae817fafcc86e000d2a100967cd5dde8a
JuanFFernan05 <jfran0505@gmail.com>
2025-01-01 17:44:12 -0300

    Agregue una simple suma donde el usuario participa

--- tarea.py
+++ tarea.py
@@ -1,1 +1,8 @@
 print("Un repositorio de prueba")
+
+# Algunas consultas - respuesta con el usuario
+
+numero1 = int(input("Ingrese un numero: "))
+numero2 = int(input("Ingrese otro numero: "))
+numeroResultante = numero1 + numero2
+print("El resultado de la suma fue =", numeroResultante)
