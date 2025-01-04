ae347f71566e5c3a6af4dec8cf571819b90a518e
JuanFFernan05 <jfran0505@gmail.com>
2025-01-04 19:29:48 -0300

    Algunos avances (pocos).

---
+++ PY/ejCuatro.py
@@ -0,0 +1,5 @@
+# A partir de una cadena pasada por parámetro, utilizar strip() para ir eliminando, secuencialmente, las vocales
+
+cadena = input("Ingrese una cadena por parámetro: ")
+
+print(cadena.strip("aAeEiIoOuU"))
